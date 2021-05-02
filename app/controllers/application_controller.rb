30f6f93e195b1900d4370def84d9f6519e3d49e2
ramsteven <ramsteven@gmail.com>
2021-05-02 17:21:47 -0500

    associations working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   set_current_tenant_through_filter
-  before_action :set_tenant
+  before_action :authenticate_user!
+  before_action :set_tenant, except: { :sessionsController => [:new,:create] } 
 
 
   def set_tenant
-    current_account = Account.where(id: current_user.current_tenant_id).first
-    set_current_tenant(current_account)
+    if current_user
+      current_account = Account.where(id: current_user.current_tenant_id).first
+      set_current_tenant(current_account)
+    end
   end
 end
